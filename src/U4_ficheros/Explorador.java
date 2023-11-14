6f916be66d9336c0bed6cda5df85581dee3fc6ff
mgg <margomgar35@alu.edu.gva.es>
2023-11-14 20:52:25 +0100

    examen

--- src/U4_ficheros/Explorador.java
+++ src/U4_ficheros/Explorador.java
@@ -1,14 +1,25 @@
 package U4_ficheros;
 import java.io.File;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Explorador {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        int exitKey = 1;
+        Scanner scanner = new Scanner(System.in);
         File f0 = File.listRoots()[0];
 
-        for(String s: f0.list()) {
-            System.out.println(s);
-        }
+        print(f0);
+
+        do {
+            System.out.println("Introduce un directorio: ");
+            String ent = nombreFichero();
+            File f = new File(f0,  ent);
+            f0 = f;
+
+            print(f);
+
+        } while (exitKey != -1);
 
     }
 
@@ -17,9 +28,29 @@ public class Explorador {
             if(f.isDirectory()) {
                 System.out.println("Lista de ficheros y directorios del directorio: " + f.getCanonicalPath());
                 System.out.println("---------------------------------------------------");
-
-                for(File e:)
+                print2(f);
             }
         }
+    }
+
+    public static void print2 (File f) {
+        int contador = 1;
+
+        for(String e : f.list()) {
+
+            if(f.isDirectory()) {
+                System.out.println(contador + "\t" + e + " <Directorio>");
+
+            } else if (f.isFile()) {
+                System.out.println(contador + "\t" + e + " " + e.length());
+            }
+
+            contador++;
+        }
+    }
+
+    public static String nombreFichero() {
+        String fichero = new Scanner(System.in).nextLine();
+        return fichero;
     }
 }
