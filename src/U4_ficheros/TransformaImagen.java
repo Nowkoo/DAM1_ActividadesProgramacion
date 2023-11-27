044e1308232afb95a0274d0c963eff950f1aeed9
mgg <margomgar35@alu.edu.gva.es>
2023-11-27 20:53:10 +0100

    flujo

--- src/U4_ficheros/TransformaImagen.java
+++ src/U4_ficheros/TransformaImagen.java
@@ -1,6 +1,6 @@
 package U4_ficheros;
 
-import java.io.File;
+import java.io.*;
 
 public class TransformaImagen {
     static File f;
@@ -11,7 +11,10 @@ public class TransformaImagen {
             System.out.println("El archivo no existe o no es .bmp");
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        new TransformaImagen(new File("./recursos/pluginfile.bmp"));
+        transformaNegativo();
+        transformaOscuro();
     }
 
     static boolean isBmp(File f) {
@@ -25,9 +28,87 @@ public class TransformaImagen {
         return false;
     }
 
-    static void transformaNegativo() {}
+    static void transformaNegativo() throws IOException {
+        File negativo = new File("./recursos/" + nombreFichero("_n"));
+        FileInputStream f_in = new FileInputStream(f);
+        FileOutputStream f_out = new FileOutputStream(negativo);
 
-    static void transformaOscuro() {}
+        byte[] cabecera = new byte[54];
+        f_in.read(cabecera);
+        f_out.write(cabecera);
 
-    static void transformaBlancoNegro() {}
+        /*for (int i = 0; i < 54; i++) {
+            num = f_in.read();
+            f_out.write(num);
+        }*/
+
+        int num;
+        num = f_in.read();
+
+        while (num != -1) {
+            f_out.write(255 - num);
+            num = f_in.read();
+        }
+
+        f_in.close();
+        f_out.close();
+    }
+
+    static void transformaOscuro() throws IOException {
+        File oscuro = new File("./recursos/" + nombreFichero("_o"));
+        FileInputStream f_in = new FileInputStream(f);
+        FileOutputStream f_out = new FileOutputStream(oscuro);
+
+        byte[] cabecera = new byte[54];
+        f_in.read(cabecera);
+        f_out.write(cabecera);
+
+        int num;
+        num = f_in.read();
+
+        while (num != -1) {
+            f_out.write(num/2);
+            num = f_in.read();
+        }
+
+        f_in.close();
+        f_out.close();
+    }
+
+    static void transformaBlancoNegro() throws IOException {
+        File blancoNegro = new File("./recursos/" + nombreFichero("_bn"));
+        FileInputStream f_in = new FileInputStream(f);
+        FileOutputStream f_out = new FileOutputStream(blancoNegro);
+
+        byte[] cabecera = new byte[54];
+        f_in.read(cabecera);
+        f_out.write(cabecera);
+
+        int num;
+        num = f_in.read();
+
+        while (num != -1) {
+            for (int i = 0; i < 3; i++) {
+                switch (i) {
+                    case 0:
+                        int red = num;
+                        break;
+                    case 1:
+                        int green = num;
+                        break;
+                    case 2:
+                        int blue = num;
+                        break;
+                }
+            }
+        }
+    }
+
+    static String nombreFichero(String n) {
+        String nombre = f.getName();
+        String nuevoNombre = nombre.substring(0, nombre.lastIndexOf('.'));
+        nuevoNombre += n + ".bmp";
+
+        return nuevoNombre;
+    }
 }
