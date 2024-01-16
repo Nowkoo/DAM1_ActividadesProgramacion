b76747ee5e531e32939ebc2f55e51d3c7f16ac7e
mgg <margomgar35@alu.edu.gva.es>
2024-01-16 20:53:37 +0100

    Hundir la flota

--- src/U5_POO/T05/Tablero.java
+++ src/U5_POO/T05/Tablero.java
@@ -1,12 +1,14 @@
 package U5_POO.T05;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Tablero {
     ArrayList<String> tablero;
     ArrayList<Barco> barcos;
     int numFilas;
     int numColumnas;
+    Random random = new Random();
 
     public Tablero(int numFilas, int numColumnas, int numBarcos) {
         this.numFilas = numFilas;
@@ -25,9 +27,29 @@ public class Tablero {
     }
 
     public void tiro(int fila, int columna) {
+        boolean tocado = false;
+        String resultado = "Has fallado.";
         InterfazUsuario ui = new InterfazUsuario();
         fila = ui.inputFila();
         columna = ui.inputColumna();
+
+        for (Barco b : barcos) {
+            if (b.fila == fila && b.columna == columna) {
+                barcos.remove(b);
+                resultado = "¡Hundido!";
+            }
+            break;
+        }
+        System.out.println(resultado);
+    }
+
+    public void actualizarTablero(int fila, int columna, boolean tocado) {
+        if (tocado) {
+            String tirada = tablero.get(fila - 1);
+            for (int i = 0; i < columna; i++) {
+
+            }
+        }
     }
 
     public void comprobarFinPartida() {
@@ -57,7 +79,7 @@ public class Tablero {
     public void generarBarcos(int numBarcos) {
         barcos = new ArrayList<>();
         for (int i = 0; i < numBarcos; i++) {
-            barcos.add(new Barco());
+            barcos.add(new Barco(random.nextInt(numFilas), random.nextInt(numColumnas)));
         }
     }
 }
