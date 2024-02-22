5d132eb24f9ab618b9ff20af63584eb03c55fddc
alu10183093 <magoga2896@gmail.com>
2024-02-22 16:49:47 +0100

    regalos

--- src/U6_estructuras_de_datos/ArrayList/RepartiendoRegalos/RepartiendoRegalos.java
+++ src/U6_estructuras_de_datos/ArrayList/RepartiendoRegalos/RepartiendoRegalos.java
@@ -1,13 +1,25 @@
 package U6_estructuras_de_datos.ArrayList.RepartiendoRegalos;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class RepartiendoRegalos {
     public void consultarPortales(int portalAterrizaje, ArrayList<Integer> portalesPendientes) {
         ArrayList<Integer> portalesOrdenados = new ArrayList<>();
-        int portalTemp;
-        int indexTemp;
-
-        for
+        int index = -1;
+        int distanciaMinima = Integer.MAX_VALUE;
+        while (!portalesPendientes.isEmpty()) {
+            for (int j = 0; j < portalesPendientes.size(); j++) {
+                int distanciaActual = Math.abs(portalAterrizaje - portalesPendientes.get(j));
+                if (distanciaMinima > distanciaActual) {
+                    distanciaMinima = distanciaActual;
+                    index = j;
+                }
+            }
+            portalesOrdenados.add(portalesPendientes.get(index));
+            portalAterrizaje = portalesPendientes.get(index);
+            portalesPendientes.remove(index);
+        }
+        System.out.println(portalesOrdenados);
     }
 }
