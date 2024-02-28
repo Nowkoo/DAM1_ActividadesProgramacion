2f3a4f0962d0b946e660a6f72bb9e8934af89e7a
alu10183093 <magoga2896@gmail.com>
2024-02-28 20:53:28 +0100

    HashMap

--- src/U6_estructuras_de_datos/Maps/Ej7_Anagramas.java
+++ src/U6_estructuras_de_datos/Maps/Ej7_Anagramas.java
@@ -1,4 +1,35 @@
 package U6_estructuras_de_datos.Maps;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Ej7_Anagramas {
+    public static void main(String[] args) throws IOException {
+        Map<String, String> diccionario = new HashMap<>();
+        BufferedReader reader = new BufferedReader(new FileReader("./recursos/spanish-dict.txt"));
+        String line;
+        while ((line = reader.readLine())!=null) {
+            diccionario.put(ordenarAlfabeticamente(line) , line);
+        }
+        reader.close();
+    }
+
+    public static String ordenarAlfabeticamente(String palabra) {
+        palabra = palabra.toLowerCase();
+        String palabraOrdenada;
+        char temp;
+        for (int i = 0; i < palabra.length() - 1; i++) {
+            for (int j = i + 1; j < palabra.length(); j++) {
+                if (palabra.charAt(i) < palabra.charAt(j)) {
+                    temp = palabra.charAt(i);
+
+                }
+            }
+            palabraOrdenada = palabraOrdenada + temp;
+        }
+        return "";
+    }
 }
