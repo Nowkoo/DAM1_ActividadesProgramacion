dd71c581713ca685f03a2896c5d0a8cc4f3066d0
alu10183093 <magoga2896@gmail.com>
2024-03-26 20:26:29 +0100

    databases

---
+++ src/U7_databases/ConnectToMySqlDriver.java
@@ -0,0 +1,31 @@
+package U7_databases;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class ConnectToMySqlDriver {
+
+    public static void main(String[] args)  {
+        String url="jdbc:mysql://localhost:33060/programacio";
+        String username = "mar";
+        String password = "secret";
+
+        try{
+            Driver driver = DriverManager.getDriver(url);
+
+            Properties properties = new Properties();
+            properties.setProperty("user", username);
+            properties.setProperty("password", password);
+
+            Connection con = driver.connect(url, properties);
+            System.out.println("Conexión completada a través de Driver");
+            con.close();
+        } catch (SQLException ex) {
+            System.out.println("Se ha producido un error al conectar: " + ex.getMessage());
+        }
+    }
+}
+
