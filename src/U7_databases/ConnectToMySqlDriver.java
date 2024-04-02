33696b2dc1515208ec0573bcb6cb6f6f6641a24f
Nowko <magoga2896@gmail.com>
2024-04-02 18:35:51 +0200

    bbdd

--- src/U7_databases/ConnectToMySqlDriver.java
+++ src/U7_databases/ConnectToMySqlDriver.java
@@ -9,10 +9,17 @@ import java.util.Properties;
 public class ConnectToMySqlDriver {
 
     public static void main(String[] args)  {
-        String url="jdbc:mysql://localhost:33060/programacio";
+        //bbdd casa
+        String url="jdbc:mysql://172.23.176.1:33060/1dam_programacio";
         String username = "mar";
         String password = "secret";
 
+        //bbdd clase
+        /*String url="jdbc:mysql://localhost:33060/programacio";
+        String username = "mar";
+        String password = "secret";*/
+
+        //bbdd bbdd
         /*String url="jdbc:postgresql://89.36.214.106:5432/factura";
         String username = "factura_alu";
         String password = "factura_alu";*/
